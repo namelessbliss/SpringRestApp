0c91ced893b199c63e905862b9ce0f4a5c15c9f0
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-11 18:01:39 -0500

    Implementando Profesor Service

---
+++ src/main/java/com/nb/spring/service/ProfesorServiceImp.java
@@ -0,0 +1,71 @@
+package com.nb.spring.service;
+
+import com.nb.spring.dao.IProfesorDao;
+import com.nb.spring.entity.Profesor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProfesorServiceImp implements IProfesorService {
+
+    @Autowired
+    private IProfesorDao profesorDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Profesor> findAll() {
+        return (List<Profesor>) profesorDao.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Profesor findProfesor(Profesor profesor) {
+        return (Profesor) profesorDao.findByEmail(profesor.getEmail());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Profesor checkProfesorLogin(Profesor profesor) {
+        return (Profesor) profesorDao.findByEmailAAndPassword(profesor.getEmail(), profesor.getPassword());
+    }
+
+    @Override
+    @Transactional
+    public void deleteProfesor(Profesor profesor) {
+        profesorDao.deleteById(profesor.getId());
+    }
+
+    @Override
+    @Transactional
+    public Profesor updateProfesor(Profesor profesor) {
+        return (Profesor) profesorDao.save(profesor);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Profesor> findProfesorById(Long profesor_id) {
+        return (Optional<Profesor>) profesorDao.findById(profesor_id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteProfesor(Long id) {
+        profesorDao.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Profesor findById(Long id) {
+        return profesorDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Profesor findByIdSQL(Long id) {
+        return profesorDao.findByIdSQL(id);
+    }
+}
