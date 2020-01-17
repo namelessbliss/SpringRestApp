122884a62d997d04e97bb3fd486287b51ccef541
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-17 16:09:12 -0500

    Correcciones en el controlador de Lenguaje

--- src/main/java/com/nb/spring/controller/LenguajeController.java
+++ src/main/java/com/nb/spring/controller/LenguajeController.java
@@ -5,14 +5,12 @@ import com.nb.spring.service.ILenguajeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class LenguajeController {
 
     @Autowired
